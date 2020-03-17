d33e79c63af72021a34b3a722271e3c0103d47fb
Arsh Mahajan <05arshmahajan@gmail.com>
2020-03-17 17:44:29 +0530

    Changed the indentation by running ctrl+B on arduino web editor

--- examples/Serial/SerialMessage.ino
+++ examples/Serial/SerialMessage.ino
@@ -15,26 +15,26 @@ CapacitiveSensor up = CapacitiveSensor(9, 8);
 
 void setup()
 {
-   Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop()
 {
   // Now we proceed to print the serial message
-   
-  if(left.capacitiveSensor(30) > 500)
+
+  if (left.capacitiveSensor(30) > 500)
   {
     Serial.print(1);
   }
-  else if(right.capacitiveSensor(30) > 500)
+  else if (right.capacitiveSensor(30) > 500)
   {
     Serial.print(2);
   }
-  else if(down.capacitiveSensor(30) > 500)
+  else if (down.capacitiveSensor(30) > 500)
   {
     Serial.print(3);
   }
-  else if(up.capacitiveSensor(30) > 500)
+  else if (up.capacitiveSensor(30) > 500)
   {
     Serial.print(4);
   }
@@ -43,8 +43,8 @@ void loop()
     Serial.print(0);
   }
   /*
-    Additionally we could do this by storing the values objects 
-    in intermediate variables that we may use while adding 
+    Additionally we could do this by storing the values objects
+    in intermediate variables that we may use while adding
     additional functionality to the sketch.
   */
 }
